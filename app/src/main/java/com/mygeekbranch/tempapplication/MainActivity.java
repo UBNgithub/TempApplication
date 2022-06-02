7e6c227ade481b15fa780cdcbf4ada38df583456
UBN100 <5423410@mail.ru>
2022-06-02 14:33:07 +0300

    Lesson 2

--- app/src/main/java/com/mygeekbranch/tempapplication/MainActivity.java
+++ app/src/main/java/com/mygeekbranch/tempapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dateText = findViewById(R.id.textViewDate);
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         dateText.setText(date.toString());
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Weather");
+        actionBar.setTitle("Moscow");
     }
 
     @Override
--- app/src/main/java/com/mygeekbranch/tempapplication/SettingActivity.java
+++ app/src/main/java/com/mygeekbranch/tempapplication/SettingActivity.java
@@ -2,10 +2,14 @@ package com.mygeekbranch.tempapplication;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class SettingActivity extends AppCompatActivity {
+    Switch mSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,13 @@ public class SettingActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Setting");
 
-
+        mSwitch = findViewById(R.id.switch2);
+        mSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mSwitch.isChecked())
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
     }
 }
