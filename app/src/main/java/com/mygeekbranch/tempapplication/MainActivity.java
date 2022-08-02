1d007160be439735f405c47a7dadf2394b20efea
UBN <5423410@mail.ru>
2022-08-02 20:32:58 +0300

    Android prod. les1. NavigationView
    добавил боковое меню NavigationView

--- app/src/main/java/com/mygeekbranch/tempapplication/MainActivity.java
+++ app/src/main/java/com/mygeekbranch/tempapplication/MainActivity.java
@@ -5,15 +5,19 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 import com.mygeekbranch.tempapplication.modelWeather.WeatherInit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements  NavigationView.OnNavigationItemSelectedListener{
     Toolbar toolbar;
 
     private static MainActivity instance;
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         instance = this;
 
 
@@ -56,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        initDrawer(toolbar);
+
 
 
         if (savedInstanceState == null) {
@@ -68,6 +74,18 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
         bottomNavigationView.setOnItemSelectedListener(itemSelectedListener2);
+
+
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
 
 
     }
@@ -129,4 +147,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Fragment selectfragment = null;
+        if (id == R.id.navigation_home){
+            selectfragment = MainFragment.newInstance(null, null);
+
+        }else  if (id == R.id.navigation_city){
+            selectfragment = CityFragment.newInstance(null, null);
+
+        }else  if (id == R.id.navigation_setting){
+            selectfragment = SettingFragment.newInstance(null, null);
+
+        }else  if (id == R.id.navigation_about){
+            selectfragment = AboutFragment.newInstance(null, null);
+
+        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container_main, selectfragment)
+                .commit();
+        DrawerLayout drawerLayout =findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+
+
+
+
+
+
+        return true;
+    }
 }
