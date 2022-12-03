ed46f85991a4e0148687d1a135c12ca9f0e26a2b
UBN_GK <boykoaleksandr82@gmail.com>
2022-12-03 11:24:47 +0300

    Les. 6 Push Firebase
    Отправка сообщения через  через сервис FireBase

--- app/src/main/java/com/mygeekbranch/tempapplication/WeatherFireBaseService.java
+++ app/src/main/java/com/mygeekbranch/tempapplication/WeatherFireBaseService.java
@@ -1,14 +1,36 @@
 package com.mygeekbranch.tempapplication;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class WeatherFireBaseService extends FirebaseMessagingService {
+    private  int messageId = 0;
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
-        super.onMessageReceived(message);
+        Log.d("WeatherMessage", message.getNotification().getBody());
+        String title = message.getNotification().getTitle();
+        if (title == null){
+            title = "Weather Message";
+        }
+
+        String text = message.getNotification().getBody();
+        // Нотификация
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "2")
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(title)
+                .setContentText(text);
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(messageId++, builder.build());
+
     }
 }
 
